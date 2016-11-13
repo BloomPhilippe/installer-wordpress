5aaf32260b50802ab1e799619e7b989a4babb174
Philippe Bloom <philippe.bloom@produweb.be>
2016-11-13 12:21:58 +0100

    ajout préfix

--- wp-installer-ask.sh
+++ wp-installer-ask.sh
@@ -1,7 +1,11 @@
 echo -n "Quel est le nom du projet ? "
 read PROJECT
+echo -n "Quel est le nom du dossier du projet ? "
+read FOLDER
 echo -n "Quel est le nom de la base de données ? "
 read DB
+echo -n "Quel est le préfix de vos tables (PAS WP_) ? "
+read PREFIX
 echo -n "Quel est le nom d'utilisateur de la base de données (souvent root)? "
 read USERNAMEDB
 echo -n "Quel est le mot de passe de la base de données ? "
@@ -15,9 +19,9 @@ read PASSWORD
 echo -n "Quel est l'URL de votre projet ? "
 read URL
 
-mkdir $PROJECT
-cd $PROJECT
+mkdir $FOLDER
+cd $FOLDER
 wp core download --locale=fr_FR --force 
-wp core config --dbname="$DB" --dbuser="$USERNAMEDB" --dbpass="$PASSWORDEDB" --skip-check
+wp core config --dbname="$DB" --dbprefix="$PREFIX" --dbuser="$USERNAMEDB" --dbpass="$PASSWORDEDB" --skip-check
 wp db create 
 wp core install --url="$URL" --title="$PROJECT" --admin_user="$USERNAME" --admin_email="$EMAIL" --admin_password="$PASSWORD"
