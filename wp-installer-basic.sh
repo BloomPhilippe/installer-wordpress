ec1f03a8458252c085546f9ca2a9d0e3c13996cb
Philippe Bloom <philippe.bloom@produweb.be>
2017-08-27 20:13:19 +0200

    add plugins

--- wp-installer-basic.sh
+++ wp-installer-basic.sh
@@ -1,7 +1,7 @@
 mkdir $1
 cd $1
 wp core download --locale=fr_FR --force
-for PLUGIN in gotmls disable-comments better-wp-security jetpack imsanity
+for PLUGIN in gotmls disable-comments better-wp-security jetpack imsanity advanced-custom-fields wp-optimize p3-profiler
 do
 	printf "installation plugin $VARIABLE"
     curl -O https://downloads.wordpress.org/plugin/$PLUGIN.zip
